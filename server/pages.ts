e4a195cba11dd8c8e7acc543388c8b37d612f055
Yohan Moore <106980605+GreenAntTech@users.noreply.github.com>
2025-03-12 22:41:27 -0400

    Update pages.ts

--- server/pages.ts
+++ server/pages.ts
@@ -1,5 +1,5 @@
 import type { ServerContext } from "../../common/server/lib/jsphere.d.ts";
-import { renderDocument$ } from "../shared/element.min.js";
+import { renderDocument$ } from "../shared/element.min1.js";
 import "../shared/dependencies.js";
 
 export async function onGET (ctx:ServerContext) : Promise<Response> {
