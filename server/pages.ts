57bd15dcb4450fa362c7b20433af4a4feb925fd2
Yohan <yohan.moore@greenantsolutions.com>
2025-03-18 15:14:39 -0400

    Updates

--- server/pages.ts
+++ server/pages.ts
@@ -4,7 +4,8 @@ import "../shared/dependencies.js";
 
 export async function onGET (ctx:ServerContext) : Promise<Response> {
     try {
-        const file = ctx.request.url.pathname.split('/')[1] || 'home';
+        // const file = ctx.request.url.pathname.split('/')[1] || 'home';
+        const file = 'console';
         const document = await renderDocument$({ file: `console/client/components/pages/${file}/${file}.html` }, ctx) as string;
         return ctx.response.html('<!doctype html>' + document);
     }
