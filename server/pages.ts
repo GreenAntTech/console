ab7481293b3fd54ed0389296ad982b9d3be42dba
Yohan Moore <106980605+GreenAntTech@users.noreply.github.com>
2025-05-27 23:11:46 -0400

    Update pages.ts

--- server/pages.ts
+++ server/pages.ts
@@ -6,7 +6,7 @@ export async function onGET (ctx:ServerContext) : Promise<Response> {
     try {
         // const file = ctx.request.url.pathname.split('/')[1] || 'home';
         const file = 'console';
-        const document = await renderDocument$({ file: `@console/client/components/pages/${file}/${file}.html` }, ctx) as string;
+        const document = await renderDocument$({ file: `console/client/components/pages/${file}/${file}.html` }, ctx) as string;
         return ctx.response.html('<!doctype html>' + document);
     }
     catch (e) {
